d09d23c7df4f28827b9260b76a832d8468138e7a
Boyarin90 <lomoti1990@gmail.com>
2024-11-28 11:51:16 +0900

    Add files via upload

---
+++ Lesson03.py
@@ -0,0 +1,8 @@
+homework=12
+time=1.5
+lesson_name='Python'
+print("Курс:",lesson_name,",","всего задач:",homework,
+      ","'затрачено часов:',time,
+      ","'среднее время выполнения:',
+      time/homework,'часа'".")
+
